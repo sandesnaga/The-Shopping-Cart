70924636433f057d0dde33da5af517f206e45d49
sandesnaga <sandesnaga@yahoo.com>
2020-04-06 15:44:19 -0500

    Created Filter Options

---
+++ TheShop/TheShop.Core/ViewModels/ProductListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheShop.Core.Models;
+
+namespace TheShop.Core.ViewModels
+{
+    public class ProductListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+
+        public IEnumerable<ProductCategory> ProductCategories { get; set; }
+    }
+}
--- TheShop/TheShop.WebUI/Controllers/HomeController.cs
+++ TheShop/TheShop.WebUI/Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using TheShop.Core.Contracts;
 using TheShop.Core.Models;
+using TheShop.Core.ViewModels;
+
 
 namespace TheShop.WebUI.Controllers
 {
@@ -18,10 +20,26 @@ namespace TheShop.WebUI.Controllers
             context = productContext;
             productCategories = productCategoryContext;
         }
-        public ActionResult Index()
+        public ActionResult Index(string Category=null)
         {
-            List<Product> products = context.Collection().ToList();
-            return View(products);
+            List<Product> products;
+            List<ProductCategory> categories = productCategories.Collection().ToList();
+            if(Category == null)
+            {
+               products = context.Collection().ToList();
+            }
+            else
+            {
+                products = context.Collection().Where(p => p.Category == Category).ToList();
+
+            }
+            ProductListViewModel model = new ProductListViewModel();
+            model.Products = products;
+            model.ProductCategories = categories;
+
+         
+
+            return View(model);
         }
         public ActionResult Details(string Id)
         {
