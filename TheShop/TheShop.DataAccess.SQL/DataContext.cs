708158074da3699baa1a2d00bef478aa41481ca8
sandesnaga <sandesnaga@yahoo.com>
2020-04-06 17:05:04 -0500

    Added basket Model

--- TheShop/TheShop.DataAccess.SQL/DataContext.cs
+++ TheShop/TheShop.DataAccess.SQL/DataContext.cs
@@ -10,12 +10,15 @@ namespace TheShop.DataAccess.SQL
 {
     public class DataContext : DbContext
     {
-        public DataContext()        
-            :base("DefaultConnection"){            
+        public DataContext()
+            : base("DefaultConnection") {
 
         }
 
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<Basket> Baskets{ get; set; }
+        public DbSet<BasketItem> BasketItems{ get; set; }
+
     }
 }
